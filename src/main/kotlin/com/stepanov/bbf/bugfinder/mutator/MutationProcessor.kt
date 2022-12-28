7d12a268d2685906ece73b0684c3856068330d71
Oleg <olezhkabobrov@gmail.com>
2022-12-28 20:17:39 +0100

    Proccessor is object, deleted all companion objects in Transformation, added some options in bbf.conf

--- src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/MutationProcessor.kt
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/MutationProcessor.kt
@@ -2,53 +2,20 @@ package com.stepanov.bbf.bugfinder.mutator
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiErrorElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.impl.source.tree.TreeElement
-import com.stepanov.bbf.bugfinder.executor.COMPILE_STATUS
 import com.stepanov.bbf.bugfinder.executor.CompilerArgs
-import com.stepanov.bbf.bugfinder.executor.checkers.CoverageGuider
-import com.stepanov.bbf.bugfinder.executor.checkers.PerformanceOracle
-import com.stepanov.bbf.bugfinder.executor.checkers.TracesChecker
 import com.stepanov.bbf.bugfinder.executor.project.BBFFile
-import com.stepanov.bbf.bugfinder.executor.project.LANGUAGE
-import com.stepanov.bbf.bugfinder.executor.project.Project
-import com.stepanov.bbf.bugfinder.manager.Bug
-import com.stepanov.bbf.bugfinder.manager.BugManager
-import com.stepanov.bbf.bugfinder.manager.BugType
-import com.stepanov.bbf.bugfinder.mutator.transformations.Factory
-import com.stepanov.bbf.bugfinder.tracer.Tracer
-import com.stepanov.bbf.bugfinder.util.StatisticCollector
 import com.stepanov.bbf.bugfinder.util.getAllParentsWithoutNode
-import com.stepanov.bbf.bugfinder.vertx.CompileRequestMessage
-import com.stepanov.bbf.bugfinder.vertx.MessageSender
-import com.stepanov.bbf.bugfinder.vertx.VertxAddresses
-import com.stepanov.bbf.reduktor.executor.KotlincInvokeStatus
-import com.stepanov.bbf.reduktor.parser.PSICreator
-import com.stepanov.bbf.reduktor.util.getAllPSIChildrenOfType
-import coverage.MyMethodBasedCoverage
-import io.vertx.core.AsyncResult
-import io.vertx.core.eventbus.EventBus
-import io.vertx.core.eventbus.Message
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.apache.log4j.Logger
-import kotlin.math.absoluteValue
+import java.io.File
 
-class MutationProcessor(
-    val project: Project,
-    val curFile: BBFFile,
-    private val eventBus: EventBus,
-    private val MutatorIndex: Int
-) {
-    val msgSender = MessageSender(eventBus)
+object MutationProcessor {
 
     /**
      * Probably unsafe.
      * Not checking if it compiles
      */
-    fun replaceNode(node: ASTNode, replacement: ASTNode, filenameOpt: String? = null): ASTNode? {
+    fun replaceNode(node: ASTNode, replacement: ASTNode, curFile: BBFFile, filenameOpt: String? = null): ASTNode? {
         log.debug("[UNSAFE] Trying to replace $node on $replacement")
         if (node.text.isEmpty() || node == replacement) {
             return node
@@ -70,7 +37,7 @@ class MutationProcessor(
                 }
                 if (CompilerArgs.shouldSaveMutatedFiles) {
                     require(filenameOpt != null)
-                    project.saveMutation(curFile, CompilerArgs.pathToMutatedDir + filenameOpt)
+                    saveMutation(curFile, CompilerArgs.pathToMutatedDir + filenameOpt)
                 }
                 return replCopy
             } catch (e: Error) {
@@ -79,8 +46,8 @@ class MutationProcessor(
         return null
     }
 
-    fun replaceNode(node: PsiElement, replacement: PsiElement, filenameOpt: String? = null): Boolean =
-        replaceNode(node.node, replacement.node, filenameOpt) != null
+    fun replaceNode(node: PsiElement, replacement: PsiElement, curFile: BBFFile, filenameOpt: String? = null): Boolean =
+        replaceNode(node.node, replacement.node, curFile, filenameOpt) != null
 
     fun checkCompiling(): Boolean {
         TODO()
@@ -95,6 +62,10 @@ class MutationProcessor(
 //        return true
     }
 
+    fun saveMutation(file: BBFFile, pathToSave: String) {
+        File(CompilerArgs.pathToMutatedDir).mkdirs()
+        File(pathToSave).writeText(file.psiFile.text)
+    }
 
     private val DUMMY_HOLDER_INDEX: Short = 86
     private val log = Logger.getLogger("mutatorLogger")
--- src/main/kotlin/com/stepanov/bbf/bugfinder/vertx/Main.kt
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/vertx/Main.kt
@@ -1,5 +1,25 @@
 package com.stepanov.bbf.bugfinder.vertx
 
+import com.stepanov.bbf.bugfinder.executor.CompilerArgs
+import io.vertx.core.Vertx
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+import java.io.File
+import kotlin.system.exitProcess
+
 fun main() {
-    println("Hello from main")
+    //Init log4j
+    PropertyConfigurator.configure("src/main/resources/bbfLog4j.properties")
+    if (!CompilerArgs.getPropAsBoolean("LOG")) {
+        Logger.getRootLogger().level = Level.OFF
+        Logger.getLogger("bugFinderLogger").level = Level.OFF
+        Logger.getLogger("mutatorLogger").level = Level.OFF
+        Logger.getLogger("reducerLogger").level = Level.OFF
+        Logger.getLogger("transformationManagerLog").level = Level.OFF
+    }
+
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(Coordinator())
+
 }
