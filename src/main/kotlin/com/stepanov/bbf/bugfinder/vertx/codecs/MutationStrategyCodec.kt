5bbda39be72021994a3ef14f73cb0a77b74601f3
Oleg <olezhkabobrov@gmail.com>
2023-01-03 17:20:50 +0100

    Add Codec for strategy. Doesn't work: Project.createFromCode() fails

---
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/vertx/codecs/MutationStrategyCodec.kt
@@ -0,0 +1,21 @@
+package com.stepanov.bbf.bugfinder.vertx.codecs
+
+import com.stepanov.bbf.bugfinder.mutator.MutationStrategy
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.eventbus.MessageCodec
+
+class MutationStrategyCodec: MessageCodec<MutationStrategy, MutationStrategy> {
+    override fun encodeToWire(buffer: Buffer?, s: MutationStrategy?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun decodeFromWire(pos: Int, buffer: Buffer?): MutationStrategy {
+        TODO("Not yet implemented")
+    }
+
+    override fun name(): String = "Mutation Strategy Codec"
+
+    override fun systemCodecID(): Byte = -1
+
+    override fun transform(s: MutationStrategy?): MutationStrategy = s ?: error("wtf why null")
+}
