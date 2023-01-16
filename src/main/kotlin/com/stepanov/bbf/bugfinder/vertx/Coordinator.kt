72eef014d42578674a879797b3cc1fe5d3087002
Oleg <olezhkabobrov@gmail.com>
2023-01-16 12:34:53 +0100

    Minor changes. Delete mutated dir + change paths

--- src/main/kotlin/com/stepanov/bbf/bugfinder/vertx/Coordinator.kt
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/vertx/Coordinator.kt
@@ -1,6 +1,7 @@
 package com.stepanov.bbf.bugfinder.vertx
 
 import com.stepanov.bbf.bugfinder.executor.CommonCompiler
+import com.stepanov.bbf.bugfinder.executor.CompilerArgs
 import com.stepanov.bbf.bugfinder.executor.compilers.JVMCompiler
 import com.stepanov.bbf.bugfinder.executor.project.Project
 import com.stepanov.bbf.bugfinder.manager.Bug
@@ -12,7 +13,6 @@ import com.stepanov.bbf.bugfinder.mutator.vertxMessages.MutationResult
 import com.stepanov.bbf.bugfinder.vertx.codecs.*
 import com.stepanov.bbf.bugfinder.vertx.information.VertxAddresses
 import com.stepanov.bbf.reduktor.executor.CompilationResult
-import com.stepanov.bbf.reduktor.executor.KotlincInvokeStatus
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.DeploymentOptions
 import io.vertx.core.eventbus.EventBus
@@ -27,6 +27,7 @@ class Coordinator: AbstractVerticle() {
 
     override fun start() {
         eb = vertx.eventBus()
+        localPreparations()
         registerCodecs()
         establishConsumers()
         deployMutators()
@@ -82,7 +83,6 @@ class Coordinator: AbstractVerticle() {
 
     private fun deployCompilers() {
         // TODO: case of several mutators
-        // TODO: not one random file
 
         val compiler = JVMCompiler()
         compilers.add(compiler)
@@ -104,7 +104,7 @@ class Coordinator: AbstractVerticle() {
     private fun getExampleStrategy(): MutationStrategy {
         // TODO: create strategy from smth
 //        val file = File(CompilerArgs.baseDir).listFiles()?.filter { it.path.endsWith(".kt") }?.random() ?: exitProcess(0)
-        val project = Project.createFromCode(File("/home/olezhka/fuzzer/tmp/arrays/MultiDeclForComponentMemberExtensions1.kt").readText())
+        val project = Project.createFromCode(File("./tmp/arrays/MultiDeclForComponentMemberExtensions1.kt").readText())
         return MutationStrategy(listOf(ExpressionReplacer(project, project.files.first(), 1)))
     }
 
@@ -114,6 +114,10 @@ class Coordinator: AbstractVerticle() {
         eb.registerDefaultCodec(CompilationResult::class.java, CompilationResultCodec())
         eb.registerDefaultCodec(Project::class.java, ProjectCodec())
         eb.registerDefaultCodec(Bug::class.java, BugCodec())
+    }
+
+    private fun localPreparations() {
+        File(CompilerArgs.pathToMutatedDir).deleteRecursively()
     }
 
     private fun workerOptions() = DeploymentOptions().setWorker(true) // TODO: exception handling, timeouts, etc
