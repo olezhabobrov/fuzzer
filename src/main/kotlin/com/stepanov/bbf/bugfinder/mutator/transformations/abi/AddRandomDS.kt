f4e1ba1c564b49b8412a2dbb84d104e83d4dfb63
Oleg <olezhkabobrov@gmail.com>
2023-01-16 18:17:14 +0100

    [Compiling] Refactored the rest

--- src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/abi/AddRandomDS.kt
+++ src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/abi/AddRandomDS.kt
@@ -1,25 +1,30 @@
 package com.stepanov.bbf.bugfinder.mutator.transformations.abi
 
-import com.stepanov.bbf.bugfinder.mutator.transformations.Factory
+import com.stepanov.bbf.bugfinder.executor.project.BBFFile
+import com.stepanov.bbf.bugfinder.executor.project.Project
 import com.stepanov.bbf.bugfinder.mutator.transformations.Transformation
 import com.stepanov.bbf.bugfinder.mutator.transformations.abi.generators.*
 import com.stepanov.bbf.bugfinder.generator.targetsgenerators.typeGenerators.RandomTypeGenerator
 import com.stepanov.bbf.reduktor.parser.PSICreator
-import com.stepanov.bbf.reduktor.util.replaceThis
 import org.jetbrains.kotlin.psi.KtFile
-import kotlin.system.exitProcess
 
-class AddRandomDS : Transformation() {
+class AddRandomDS(project: Project, file: BBFFile,
+                  amountOfTransformations: Int = 1, probPercentage: Int = 100):
+    Transformation(project, file,
+        amountOfTransformations, probPercentage) {
 
-    private var ctx = PSICreator.analyze(file)!!
+    init {
+        updateCtx()
+    }
+
     var tries = 14
     var enoughClasses = 7
 
     private fun update() {
-        checker.curFile.changePsiFile(file.text)
-        ctx = PSICreator.analyze(file)!!
-        GeneratorFactory.update(file as KtFile, ctx)
-        RandomTypeGenerator.setFileAndContext(file as KtFile, ctx)
+        file.changePsiFile(file.text)
+        ctx = PSICreator.analyze(file.psiFile)!!
+        GeneratorFactory.update(file.psiFile as KtFile, ctx!!)
+        RandomTypeGenerator.setFileAndContext(file.psiFile as KtFile, ctx!!)
     }
 
     override fun transform() {
@@ -30,17 +35,7 @@ class AddRandomDS : Transformation() {
             val generator = GeneratorFactory.getRandomGenerator()
             //val generator = RandomClassGenerator(file as KtFile, ctx)
             val addedPsiElement = generator.generateAndAddToFile() ?: continue
-            val res = checker.checkCompiling()
-            //println("RES = $res")
-            if (!res) {
-                //println("CANT COMPILE =((\n${file.text}\n_______________________________________")
-                val psiWhiteSpace = Factory.psiFactory.createWhiteSpace("\n")
-                addedPsiElement.replaceThis(psiWhiteSpace)
-                if (!checker.checkCompiling()) {
-                    println(file.text)
-                    exitProcess(0)
-                }
-            } else if (generator is AbstractClassGenerator) successClasses++
+            if (generator is AbstractClassGenerator) successClasses++
             if (successClasses == enoughClasses) break
         }
     }
