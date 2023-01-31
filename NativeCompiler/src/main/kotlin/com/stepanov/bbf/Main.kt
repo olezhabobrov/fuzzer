efb8726b304974ba8917fcd74e045939b6b2bf35
Oleg <olezhkabobrov@gmail.com>
2023-01-31 17:50:27 +0100

    Super naive implementation of NativeCompiler

--- NativeCompiler/src/main/kotlin/com/stepanov/bbf/Main.kt
+++ NativeCompiler/src/main/kotlin/com/stepanov/bbf/Main.kt
@@ -1,8 +1,20 @@
 package com.stepanov.bbf
 
+import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager
+import org.apache.log4j.PropertyConfigurator
 import org.jetbrains.kotlin.cli.bc.K2Native
 
 fun main() {
-    K2Native()
-    println("hello from native")
+    PropertyConfigurator.configure("log4j.properties")
+    val manager = HazelcastClusterManager()
+    Vertx.clusteredVertx(VertxOptions().setClusterManager(manager)) { res ->
+        if (res.succeeded()) {
+            val vertx = res.result()
+            vertx.deployVerticle(NativeCompiler())
+        } else {
+            error("Failed: " + res.cause())
+        }
+    }
 }
