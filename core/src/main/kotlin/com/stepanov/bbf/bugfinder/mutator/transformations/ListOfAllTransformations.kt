0d22ce9ddef32df6cd7309bfb2967a505e657525
Oleg <olezhkabobrov@gmail.com>
2023-02-08 00:09:20 +0100

    Send several mutation strategies

--- core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/ListOfAllTransformations.kt
+++ core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/ListOfAllTransformations.kt
@@ -5,8 +5,13 @@ import kotlin.reflect.KClass
 object Constants {
     // TODO: all transformation classes should be here
     val allTransformations: List<KClass<out Transformation>> = listOf(
+        AddArgumentToFunction::class,
+        AddBlockToExpression::class,
+        AddBracketsToExpression::class,
+        AddCallableReference::class,
         ExpressionObfuscator::class,
         ExpressionReplacer::class,
-        ChangeConstants::class
+        ChangeConstants::class,
+
     )
 }
