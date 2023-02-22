08314b6cef8cdea207f18954d8480ad278ebc057
Oleg <olezhkabobrov@gmail.com>
2023-02-22 18:07:20 +0100

    Refactored MutationProblem. Now we have different classes for Random file, Source file etc.

--- core/src/main/kotlin/com/stepanov/bbf/bugfinder/server/messages/MutationProblem.kt
+++ core/src/main/kotlin/com/stepanov/bbf/bugfinder/server/messages/MutationProblem.kt
@@ -1,11 +1,9 @@
 package com.stepanov.bbf.bugfinder.server.messages
 
-import com.stepanov.bbf.information.CompilerArgs
 import com.stepanov.bbf.bugfinder.project.Project
 import com.stepanov.bbf.bugfinder.mutator.transformations.Constants
 import com.stepanov.bbf.bugfinder.mutator.transformations.Transformation
 import com.stepanov.bbf.bugfinder.mutator.vertxMessages.MutationStrategy
-import com.stepanov.bbf.bugfinder.util.getRandomVariableName
 import com.stepanov.bbf.information.VertxAddresses
 import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
@@ -24,16 +22,15 @@ data class MutationProblem(
 ) {
     fun createMutationStrategy(): MutationStrategy {
         val project: Project
-        if (mutationTarget is FileTarget) {
-            mutationTarget.writeFile()
-            project = Project(listOf(mutationTarget.realFileName()))
-        } else if (mutationTarget is ProjectTarget) {
-            mutationTarget.writeProject()
-            project = Project(mutationTarget.files.map { file ->
-                mutationTarget.realFileTarget(file).realFileName()
-            })
-        } else {
-            error("mutationTarget is not file or project wtf")
+        when (mutationTarget) {
+            is SingleSourceTarget -> {
+                mutationTarget.writeFile()
+                project = Project(listOf(mutationTarget.getLocalName()))
+            }
+            is ProjectTarget -> {
+                mutationTarget.writeProject()
+                project = Project(mutationTarget.files.map { it.getLocalName() })
+            }
         }
         // TODO: probably shouldn't mutate random file
         // i.e. for a certain mutation we should mutate certain file
@@ -70,7 +67,6 @@ data class MutationProblem(
         if (listOfTransformations.isEmpty()) {
             throw IllegalArgumentException("List of allowed transformations is empty")
         }
-        mutationTarget.validate()
     }
 }
 
@@ -89,88 +85,66 @@ data class TransformationsList(val transformationsList: List<TransformationClass
 data class TransformationClass(val clazz: KClass<out Transformation>)
 
 @Serializable
-sealed class MutationTarget {
-    open fun validate() {
-        throw IllegalArgumentException("Mutation target should be project or file")
+sealed class MutationTarget
+
+@Serializable
+sealed class SingleSourceTarget: MutationTarget() {
+
+    abstract fun getSourceCode(): String
+
+    abstract fun getLocalName(): String
+
+    fun writeFile() {
+        val fileName = getLocalName()
+        File(fileName.substringBeforeLast("/")).mkdir()
+            File(fileName).writeText(getSourceCode())
     }
 }
 
-/**
- *  If fileName and code are both null, then should choose random file from tmp folder
- */
 @Serializable
-@SerialName("file")
-data class FileTarget(
-    val fileName: String? = null,
-    val code: String? = null
-): MutationTarget() {
-    fun realFileName(): String {
-        val tmpFolder = "projectTmp/${Random().getRandomVariableName()}/"
-        if (fileName == null) {
-            return tmpFolder + "tmp.kt"
-        }
-        if (!File(fileName).exists()) {
-            return tmpFolder + fileName
-        }
-        return tmpFolder + fileName.removePrefix("tmp/arrays/")
-    }
+@SerialName("random")
+class RandomFileTarget: SingleSourceTarget() {
+    private val tmpFileName =
+        File("tmp/arrays/").listFiles()?.filter { it.path.endsWith(".kt") }?.random()!!.name
+    private val code = File("tmp/arrays/$tmpFileName").readText()
+    private val name =
+        "projectTmp/$tmpFileName"
 
-    fun writeFile() {
-        val realFileName = realFileName()
-        File(realFileName.substringBeforeLast("/")).mkdir()
-        if (code != null)
-            File(realFileName).writeText(code)
-        else
-            File(realFileName).writeText(
-                File(fileName!!).readText()
-            )
-    }
+    override fun getLocalName(): String = name
 
-//    fun extactCode(): String {
-//        if (code != null)
-//            return code
-//        if (fileName != null)
-//            return File(fileName).readText()
-//        val file = File(CompilerArgs.baseDir).listFiles()?.filter { it.path.endsWith(".kt") }?.random()
-//            ?: error("wtf couldn't choose random file for some reason")
-//        return file.readText()
-//    }
+    override fun getSourceCode(): String = code
+}
 
-    override fun validate() {
-        if (code == null &&
-            fileName != null &&
-            !File(fileName).exists())
-            throw IllegalArgumentException("should provide either code of fuzzable file or it correct name")
+@Serializable
+@SerialName("code")
+class SourceFileTarget(val code: String): SingleSourceTarget() {
+    override fun getLocalName(): String = "projectTmp/tmp.kt"
+    override fun getSourceCode(): String = code
+}
 
-    }
+@Serializable
+@SerialName("name")
+class NameFileTarget(val name: String): SingleSourceTarget() {
+    private val copiedFileName =
+        "projectTmp/$name"
+    private val code = File("tmp/arrays/$name").readText()
+
+    override fun getLocalName(): String = copiedFileName
+
+    override fun getSourceCode(): String = code
 }
 
 @Serializable
 @SerialName("project")
 data class ProjectTarget(
-    val projectRoot: String,
-    val files: List<FileTarget>,
+//    val projectRoot: String,
+    val files: List<NameFileTarget>,
     val args: String = ""
 ): MutationTarget() {
-    override fun validate() {
-        if (files.isEmpty())
-            throw IllegalArgumentException("No files in project")
-        files.forEach {  file ->
-            if (file.fileName == null) {
-                throw IllegalArgumentException("Provided project, but fileName not provided wtf")
-            }
-            realFileTarget(file).validate()
-        }
-    }
 
     fun writeProject() {
-        files.forEach { file ->
-            realFileTarget(file).writeFile()
-        }
+        files.forEach { it.writeFile() }
     }
-
-    fun realFileTarget(file: FileTarget): FileTarget =
-        FileTarget(File(File(projectRoot), file.fileName!!).path, file.code)
 }
 
 fun parseMutationProblem(data: String): MutationProblem =
