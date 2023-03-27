46bc8b64b32e717642d0813a356c90a62cad0d39
Oleg <olezhkabobrov@gmail.com>
2023-03-27 17:19:10 +0200

    Refactoring coordinator

--- CommonCompiler/src/main/kotlin/com/stepanov/bbf/messages/CompilationRequest.kt
+++ CommonCompiler/src/main/kotlin/com/stepanov/bbf/messages/CompilationRequest.kt
@@ -1,12 +1,9 @@
 package com.stepanov.bbf.messages
 
-import com.stepanov.bbf.information.CompilationConfiguration
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CompilationRequest(
-    val projectMessage: ProjectMessage,
-    val configuration: CompilationConfiguration,
-    val logInfo: String,
+    val projectMessage: List<ProjectMessage>,
     val mutationNumber: Int
 )
--- core/src/main/kotlin/com/stepanov/bbf/bugfinder/coordinator/Coordinator.kt
+++ core/src/main/kotlin/com/stepanov/bbf/bugfinder/coordinator/Coordinator.kt
@@ -4,15 +4,16 @@ import com.stepanov.bbf.CommonCompiler
 import com.stepanov.bbf.bugfinder.manager.Bug
 import com.stepanov.bbf.bugfinder.manager.BugManager
 import com.stepanov.bbf.bugfinder.mutator.Mutator
+import com.stepanov.bbf.bugfinder.mutator.transformations.FTarget
+import com.stepanov.bbf.bugfinder.mutator.transformations.Transformation
 import com.stepanov.bbf.bugfinder.mutator.transformations.tce.StdLibraryGenerator
+import com.stepanov.bbf.bugfinder.mutator.vertxMessages.MutationRequest
 import com.stepanov.bbf.bugfinder.mutator.vertxMessages.MutationResult
 import com.stepanov.bbf.bugfinder.mutator.vertxMessages.MutationStrategy
-import com.stepanov.bbf.bugfinder.reducer.ResultsFilter
+import com.stepanov.bbf.bugfinder.project.Project
 import com.stepanov.bbf.bugfinder.server.codecs.*
 import com.stepanov.bbf.bugfinder.server.messages.CompilationResultHolder
-import com.stepanov.bbf.bugfinder.server.messages.CompilationResultsProcessor
 import com.stepanov.bbf.bugfinder.server.messages.MutationProblem
-import com.stepanov.bbf.bugfinder.server.messages.parseMutationProblem
 import com.stepanov.bbf.codecs.CompilationRequestCodec
 import com.stepanov.bbf.codecs.CompilationResultCodec
 import com.stepanov.bbf.information.CompilationConfiguration
@@ -24,18 +25,15 @@ import com.stepanov.bbf.messages.ProjectMessage
 import com.stepanov.bbf.reduktor.parser.PSICreator
 import io.vertx.core.DeploymentOptions
 import io.vertx.core.eventbus.EventBus
-import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import org.apache.log4j.Logger
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-class Coordinator(
-    val mutationProblem: MutationProblem
-): CoroutineVerticle() {
+class Coordinator: CoroutineVerticle() {
 
     private lateinit var eb: EventBus
+    private lateinit var transformationIterator: Iterator<Transformation>
 
     override suspend fun start() {
         eb = vertx.eventBus()
@@ -52,82 +50,73 @@ class Coordinator(
             log.debug("Consumer got parsed MutationProblem")
             val mutationProblem = msg.body()
             val strategy = mutationProblem.createMutationStrategy()
+            transformationIterator = strategy.transformations.iterator()
             log.debug("Created mutation strategy: $strategy")
             strategiesMap[strategy.number] = strategy
-            sendStrategyAndMutate(strategy)
+            startStrategyAndMutate(strategy)
         }
 
         eb.consumer<CompilationResult>(VertxAddresses.compileResult) { result ->
             log.debug("Got compilation result")
             val compileResult = result.body()
-            if (compileResult.invokeStatus.hasCompilerCrash()) {
-                log.debug("Found some bug")
-                log.debug("Bug recreated by: ${compileResult.request.logInfo}")
+            val projectsToSend = mutableListOf<Project>()
+            compileResult.results.forEach { status ->
+                if (status.isCompileSuccess) {
+                    projectsToSend.add(status.projectMessage)
+                }
+                if (status.hasCompilerCrash()) {
+                    log.debug("Found some bug")
+                    TODO()
+                }
             }
-            if (compilationResultsProcessor.shouldSendToBugManager(compileResult.request.mutationNumber)) {
-                log.debug("Sending results to BugManager")
-                vertx.eventBus().send(
-                    VertxAddresses.bugManager, CompilationResultHolder(
-                        compilationResultsProcessor.getCompilationResults(compileResult.request.mutationNumber)
-                    )
-                )
-                compilationResultsProcessor.removeProject(compileResult.request.mutationNumber)
-            }
+            sendNextTransformation(projectsToSend, compileResult.)
+
+
+//                vertx.eventBus().send(
+//                    VertxAddresses.bugManager, CompilationResultHolder(
+//                        compilationResultsProcessor.getCompilationResults(compileResult.request.mutationNumber)
+//                    )
+//                )
         }
 
         eb.consumer<MutationResult>(VertxAddresses.mutationResult) { result ->
             val mutationResult = result.body()
             log.debug("Got mutationResult with ${mutationResult.projects.size} results")
-            sendProjectToCompilers(mutatedProject)
-            if (mutatedProject.isFinal) {
-                log.debug("Got completed mutation result by strategy#${mutatedProject.strategyNumber}")
-                val strategy = strategiesMap[mutatedProject.strategyNumber]!!
+            sendProjectToCompilers(mutationResult)
+            if (!transformationIterator.hasNext()) {
+                log.debug("Got completed mutation result by strategy#${mutationResult.strategyNumber}")
+                val strategy = strategiesMap[mutationResult.strategyNumber]!!
 //                FooBarCompiler.tearDownMyEnv(strategy.project.env)
                 val mutationProblem = strategiesMap.remove(strategy.number)!!.mutationProblem
                 if (mutationProblem.repeatInf)
-                    sendMutationProblem(mutationProblem)
+                    eb.send(VertxAddresses.mutationProblemExec, mutationProblem)
             }
         }
     }
 
-    private fun sendStrategyAndMutate(strategy: MutationStrategy) {
-        log.debug("Sending strategy#${strategy.number}")
-        eb.send(VertxAddresses.mutate, strategy)
+    private fun startStrategyAndMutate(strategy: MutationStrategy) {
+        log.debug("Starting strategy#${strategy.number}")
+        sendNextTransformation(listOf(strategy.project), strategy.number)
+    }
+
+    private fun sendNextTransformation(projects: List<Project>, strategyNumber: Int) {
+        if (transformationIterator.hasNext()) {
+            val transformation = transformationIterator.next()
+            eb.send(VertxAddresses.mutate,
+                MutationRequest(transformation,
+                    projects.map { FTarget(it, it.files.random()) },
+                    strategyNumber)
+            )
+        }
     }
 
     private fun sendProjectToCompilers(mutationResult: MutationResult) {
-
         log.debug("Sending project to compiler after/while mutating by strategy#${mutationResult.strategyNumber}")
         val compilers = strategiesMap[mutationResult.strategyNumber]!!.mutationProblem.compilers
         compilers.forEach { address ->
-            CommonCompiler.compilerToConfigMap[address]?.forEach config@{ config ->
-                val project = getProjectMessageByConfig(mutationResult.projects, config)
-                if (project == null) {
-                    log.warn("Couldn't find projectMessage for configuration $config")
-                    return@config
-                }
-                if (project in checkedProjects) {
-                    log.debug("Received project has already been compiled, going back")
-                    return@config
-                }
-                checkedProjects.add(project)
-
-                compilationResultsProcessor.increaseCounter(mutationResult.mutationNumber)
-                // TODO: achtung, it's incorrect, ask Marat how to do that
-                eb.send(address,
-                    CompilationRequest(project, config, mutationResult.logInfo(), mutationResult.mutationNumber)
-                )
-            }
-        }
-
-    }
-
-    private fun getProjectMessageByConfig(projects: List<ProjectMessage>,
-                                          config: CompilationConfiguration
-    ): ProjectMessage? {
-        return when (config) {
-            CompilationConfiguration.Split -> projects.firstOrNull { it.isSplit }
-            else -> projects.firstOrNull { !it.isSplit }
+            eb.send(address,
+                CompilationRequest(mutationResult.projects.toList(), mutationResult.mutationNumber)
+            )
         }
     }
 
