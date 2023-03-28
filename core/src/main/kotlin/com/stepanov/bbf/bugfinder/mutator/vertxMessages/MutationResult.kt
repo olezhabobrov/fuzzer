86dadf46c5df0346484840be0f2be586e7212e86
Oleg <olezhkabobrov@gmail.com>
2023-03-28 15:49:50 +0200

    Add FilePartition in Transformation

--- core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/vertxMessages/MutationResult.kt
+++ core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/vertxMessages/MutationResult.kt
@@ -1,8 +1,6 @@
 package com.stepanov.bbf.bugfinder.mutator.vertxMessages
 
-import com.stepanov.bbf.bugfinder.project.Project
 import com.stepanov.bbf.messages.ProjectMessage
-import org.jetbrains.kotlin.backend.common.push
 import java.util.concurrent.atomic.AtomicInteger
 
 data class MutationResult(
@@ -16,12 +14,6 @@ data class MutationResult(
 
         val mutationCounter = AtomicInteger(0)
 
-        fun createProjects(project: Project): List<ProjectMessage> {
-            val projects = mutableListOf<ProjectMessage>()
-            projects.push(project.createProjectMessage())
-            projects.push(project.splitProject())
-            return projects
-        }
     }
 
 }
