ffe289b6b14d9b6c037646293fa72877f7ade438
Oleg <olezhkabobrov@gmail.com>
2023-04-08 21:33:10 +0200

    Fix AddPossibleModifiers

--- core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/AddPossibleModifiers.kt
+++ core/src/main/kotlin/com/stepanov/bbf/bugfinder/mutator/transformations/AddPossibleModifiers.kt
@@ -9,36 +9,30 @@ import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.KtProperty
 import java.util.*
 
-class AddPossibleModifiers: Transformation() {
+class AddPossibleModifiers: Transformation(5) {
 
     override fun transform(target: FTarget) {
         val file = target.file
-        val values = file.psiFile.node.getAllChildrenNodes()
-                .asSequence()
+        val value = file.psiFile.node.getAllChildrenNodes()
                 .filter { it.elementType == KtNodeTypes.CLASS || it.elementType == KtNodeTypes.PROPERTY
                         || it.elementType == KtNodeTypes.FUN }
-                .filter { getRandomBoolean(4) }
-                .toList()
-        for (i in 0..randomConstant) {
-            values.forEach {
-                val curWorkingList =
-                    when (it.elementType) {
-                        KtNodeTypes.CLASS -> possibleClassModifiers
-                        KtNodeTypes.PROPERTY -> possiblePropertyModifiers
-                        else -> possibleFunctionModifiers
-                    }
-                val el =
-                    when (it.elementType) {
-                        KtNodeTypes.CLASS -> it.psi as KtClass
-                        KtNodeTypes.PROPERTY -> it.psi as KtProperty
-                        else -> it.psi as KtFunction
-                    }
-                val num = Random().nextInt(curWorkingList.size)
-                val keyword = KtTokens.MODIFIER_KEYWORDS_ARRAY.find { it.value == curWorkingList[num] } ?: return@forEach
-                if (el.hasModifier(keyword)) return@forEach
-                el.addModifier(keyword)
+                .random()
+        val curWorkingList =
+            when (value.elementType) {
+                KtNodeTypes.CLASS -> possibleClassModifiers
+                KtNodeTypes.PROPERTY -> possiblePropertyModifiers
+                else -> possibleFunctionModifiers
             }
-        }
+        val el =
+            when (value.elementType) {
+                KtNodeTypes.CLASS -> value.psi as KtClass
+                KtNodeTypes.PROPERTY -> value.psi as KtProperty
+                else -> value.psi as KtFunction
+            }
+        val num = Random().nextInt(curWorkingList.size)
+        val keyword = KtTokens.MODIFIER_KEYWORDS_ARRAY.find { it.value == curWorkingList[num] } ?: return
+        if (el.hasModifier(keyword)) return
+        el.addModifier(keyword)
     }
 
     private val possibleClassModifiers = listOf("private", "protected", "internal", "public", "open", "inner", "sealed",
@@ -48,7 +42,5 @@ class AddPossibleModifiers: Transformation() {
             "public", "protected", "internal", "const")
 
     private val possibleFunctionModifiers = listOf("tailrec", "operator", "infix", "external") //"suspend"
-
-    private val randomConstant = 25
 
 }
